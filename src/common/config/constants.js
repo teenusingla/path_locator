686612f1f97fea2b198e37efe5e01e17e6afdd85
Teenu Singla <teenusingla25@gmail.com>
2018-12-19 11:24:26 +0530

    change in .env file and redme

--- src/common/config/constants.js
+++ src/common/config/constants.js
@@ -1,6 +1,6 @@
-//export const googleAPIKey = process.env.GMAP_KEY;
+export const googleAPIKey = process.env.REACT_APP_GOOGLE_API_KEY;
 
-export const googleAPIKey = "AIzaSyBG0SybP0EKWH3Jvwki7IR5AMyO_cUeeQc";
+//export const googleAPIKey = "AIzaSyBG0SybP0EKWH3Jvwki7IR5AMyO_cUeeQc";
 
 //export const googleAPIKey = 'AIzaSyDJQZ2UWHIYFXOoDQrzk4GUymhcXvopTM4'; //`AIzaSyBG0SybP0EKWH3Jvwki7IR5AMyO_cUeeQc`;
 
